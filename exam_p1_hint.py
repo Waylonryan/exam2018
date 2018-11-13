b9816d786672f3639ffce8b1f18b4e7b4ecdf0f2
Zhi Li <zli@babson.edu>
2018-11-13 15:27:13 -0500

    Update exam_p1_hint.py

---
+++ exam_p1_hint.py
@@ -0,0 +1,32 @@
+def get_value(name):
+    # return the value of given name
+    return
+
+
+def get_name_list(file_name):
+    name_list = []
+    # load the txt file, get all the first names to name_list
+    return name_list
+
+
+def who_has_highest_value(name_list):
+    # return the name with highest value among the given name_list
+    pass
+
+
+def get_words(file_name):
+    # return a list of words
+    return
+
+
+def get_words_with_same_value(word_list, value):
+    # return a list of matched words
+    return
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
